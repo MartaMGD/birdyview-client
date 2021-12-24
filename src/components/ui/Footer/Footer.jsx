5cafffb4e1d9714940fa7bb1e3584d3396e4196a
Marta Gonzalez <martagd96@gmail.com>
2021-12-24 22:54:47 +0100

    Footer modificado

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <div className="footerStyle">
-            
+            <span>Marta González Duque ® - 2021 / 2022</span>
         </div>
     )
 }
