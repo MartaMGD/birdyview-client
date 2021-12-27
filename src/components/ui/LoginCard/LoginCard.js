954f419bdb0fd687e7ab18fded35c84b40efe2da
Marta Gonzalez <martagd96@gmail.com>
2021-12-27 11:31:53 +0100

    Añadido efecto visual al formulario de registro y login

--- src/components/ui/LoginCard/LoginCard.js
+++ src/components/ui/LoginCard/LoginCard.js
@@ -5,36 +5,41 @@ export default function LoginCard(props) {
     return (
         <main>
             <form className="loginRegisterCard">
-                <span className="loginMessage">{props.loginMessage}</span>
 
-                <div>
-                    <input className="loginInputStyle"
-                        type="text"
-                        placeholder={props.firstInputText} />
+                <div className="cardImage"></div>
+
+                <div className="formContentWrap">
+                    <span className="loginMessage">{props.loginMessage}</span>
+
+                    <div>
+                        <input className="loginInputStyle"
+                            type="text"
+                            placeholder={props.firstInputText} />
+                    </div>
+
+                    <div>
+                        <input className="loginInputStyle"
+                            type="text"
+                            placeholder={props.secondInputText} />
+
+                    </div>
+
+                    <span className="loginSpanStyle">
+                        <Link
+                            to="/recuperarpassword">
+                            ¿Has olvidado tu contraseña?
+                        </Link>
+                    </span>
+
+                    <span className="loginSpanStyle">
+                        <Link
+                            to="/registernewuser">
+                            ¿Aún no tienes una cuenta? Regístrate
+                        </Link>
+                    </span>
+
+                    <input className="loginButtonStyle" type="submit" value="Enviar" />
                 </div>
-
-                <div>
-                    <input className="loginInputStyle"
-                        type="text"
-                        placeholder={props.secondInputText} />
-
-                </div>
-
-                <span className="loginSpanStyle">
-                    <Link
-                        to="/recuperarpassword">
-                        ¿Has olvidado tu contraseña?
-                    </Link>
-                </span>
-
-                <span className="loginSpanStyle">
-                    <Link
-                    to="/registernewuser">
-                    ¿Aún no tienes una cuenta? Regístrate
-                    </Link>
-                </span>
-
-                <input className="loginButtonStyle" type="submit" value="Enviar" />
             </form>
         </main>
     )
--- src/components/ui/RegisterCard/RegisterCard.js
+++ src/components/ui/RegisterCard/RegisterCard.js
@@ -2,34 +2,39 @@ import '../../../css/LoginCard.css';
 
 export default function RegisterCard() {
     return (
-        <main>
+        <main className="loginWrapper">
             <form className="loginRegisterCard">
-                <span className="loginMessage">Crea una cuenta</span>
 
-                <div>
-                    <span className="inputTextPlaceholders">Nombre de usuario:</span>
-                    <input className="loginInputStyle"
-                        type="text"
-                        placeholder="Crea un nombre de usuario" />
+                <div className="cardImage"></div>
+
+                <div className="formContentWrap">
+                    <span className="registerMessage">Crea una cuenta</span>
+
+                    <div>
+                        <span className="inputTextPlaceholders">Nombre de usuario: </span>
+                        <input className="loginInputStyle"
+                            type="text"
+                            placeholder="Crea un nombre de usuario" />
+                    </div>
+
+                    <div>
+                        <span className="inputTextPlaceholders">Correo electrónico: </span>
+                        <input className="loginInputStyle"
+                            type="email"
+                            placeholder="Introduce tu correo electrónico" />
+                    </div>
+
+
+                    <div>
+                        <span className="inputTextPlaceholders">Contraseña: </span>
+                        <input className="loginInputStyle"
+                            type="password"
+                            placeholder="Crea una contraseña" />
+                    </div>
+
+                    <input className="loginButtonStyle" type="submit" value="Enviar" />
                 </div>
-
-                <div>
-                    <span className="inputTextPlaceholders">Correo electrónico:</span>
-                    <input className="loginInputStyle"
-                        type="email"
-                        placeholder="Introduce tu correo electrónico" />
-                </div>
-
-
-                <div>
-                    <span className="inputTextPlaceholders">Contraseña:</span>
-                    <input className="loginInputStyle"
-                        type="password"
-                        placeholder="Crea una contraseña" />
-                </div>
-
-                <input className="loginButtonStyle" type="submit" value="Enviar" />
             </form>
-        </main>
+        </main >
     )
 }
