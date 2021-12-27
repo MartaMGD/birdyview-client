7d637865805a004fdf0d96d08626a27ad56a936f
Marta Gonzalez <martagd96@gmail.com>
2021-12-27 14:32:39 +0100

    Cambios esteticos home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,10 +9,9 @@ export default function Home() {
             <Hero/>
             {/* Text Box */}
             <p className="heroTextBox">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit nesciunt pariatur consectetur in? Corrupti nihil ullam molestiae 
-                facilis sequi cumque illum et culpa ut rem quisquam ab odio, magni suscipit recusandae quae officiis nulla necessitatibus! Quia, 
-                officia quos harum beatae ex culpa provident expedita facere, veniam quaerat sunt ipsa nisi. Consequuntur incidunt dolorum provident 
-                ullam voluptatibus excepturi, amet distinctio fuga obcaecati. 
+                Descubre la diversa fauna ornitológica de Andalucía a través de nuestro buscador personalizado, 
+                entérate de las últimas noticias sobre observación de aves y actualiza, comparte y 
+                comenta los últimos avistamientos realizados por otros aficionados.
             </p>
             <HomeInfoBox/>
             <NewsletterInput/>
