b57a2c70825e5bebbbeeefc8bb98c5f888cc0ee4
Marta Gonzalez <martagd96@gmail.com>
2021-12-27 12:49:23 +0100

    Estilos provisionales aplicados a blog y avistamientos

--- src/views/Articles.jsx
+++ src/views/Articles.jsx
@@ -1,8 +1,9 @@
+import '../css/globalstyle.css';
 
 export default function Articles() {
     return (
         <main className="container">
-            <h1>Blog</h1>
+            <h2 className="titleStyle"> Últimos artículos </h2>
         </main>
     )
 }
--- src/views/Birdwatching.jsx
+++ src/views/Birdwatching.jsx
@@ -1,8 +1,9 @@
+import '../css/globalstyle.css';
 
 export default function Birdwatching() {
     return (
         <main className="container">
-            <h1>Avistamientos</h1>
+            <h1 className="titleStyle">Avistamientos</h1>
         </main>
     )
 }
