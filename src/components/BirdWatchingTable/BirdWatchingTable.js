f9450e8c0a42af6fbb759d91a8d9aa1d42fea85f
Marta Gonzalez <martagd96@gmail.com>
2021-12-31 20:58:11 +0100

    Input handle en avistamientos

--- src/components/BirdWatchingTable/BirdWatchingTable.js
+++ src/components/BirdWatchingTable/BirdWatchingTable.js
@@ -1,25 +1,51 @@
-import '../../css/BirdWatchingTable.css';
+import { useState } from 'react';
 
-export default function BirdwatchingTable({birds, setBirds}) {
+export default function BirdwatchingTable() {
+    const [birds, setBirds] = useState([])
+    const [bird, setBird] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const newBird = {
+            id: "",
+            text: bird,
+            completed: false
+        }
+
+        setBirds([...birds].concat(newBird))
+        setBird("")
+    }
 
     return (
         <>
-            <table id="tableStyle">
-                <thead>
-                    <tr>
-                        <th>Ave</th>
-                        {/* <th>Localización</th>
-                        <th>Hora</th> */}
-                        {/* <th>¿Fotografiado?</th> */}
-                    </tr>
-                </thead>
-                <tbody>
-                        <tr>
-                            {/* <td>¿Será el problema que no sé que meter aquí?</td> */}
-                        
-                        </tr>
-                </tbody>
-            </table>
+            <div>
+                <form onSubmit={handleSubmit}>
+
+                    <input type="text"
+                        placeholder="Nombre del ave"
+                        onChange={(e) => setBird(e.target.value)}
+                        value={bird} />
+
+                    <button type="submit">Añadir</button>
+                    {birds.map((bird) => <li key={bird.id}>
+                        {bird.text}
+                    </li>)}
+
+                    <input type="text"
+                        placeholder="Nombre del ave"
+                        onChange={(e) => setBird(e.target.value)}
+                        value={bird} />
+
+                    <button type="submit">Añadir</button>
+                    {birds.map((bird) => <li key={bird.id}>
+                        {bird.text}
+                    </li>)}
+                </form>
+            </div>
+
         </>
-    )
+
+
+    );
 }
--- src/data/birdwatchingdata.js
+++ src/data/birdwatchingdata.js
@@ -3,28 +3,28 @@ export const birdviews = [
         "id": "1",
         "watchedbird": "Lavandera Cascadeña",
         "location": "Málaga",
-        "timewatched": "16:30",
+        "localhour": "16:30",
         "photographed": "Sí"
     },
     {
         "id": "2",
         "watchedbird": "Buitre Leonado",
         "location": "Sevilla",
-        "timewatched": "12:45",
+        "localhour": "12:45",
         "photographed": "No"
     },
     {
         "id": "3",
         "watchedbird": "Abejero Europeo",
         "location": "Málaga",
-        "timewatched": "18:00",
+        "localhour": "18:00",
         "photographed": "No"
     },
     {
         "id": "4",
         "watchedbird": "Escribano Montesino",
         "location": "Jaén",
-        "timewatched": "10:15",
+        "localhour": "10:15",
         "photographed": "Sí"
     }
 ]
