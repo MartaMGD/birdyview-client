fe8677da11771bcbff547ba941c4da8c77694a01
Marta Gonzalez <martagd96@gmail.com>
2022-01-12 18:12:55 +0100

    Tabla de pajaros funcional

--- src/views/Birdwatching.jsx
+++ src/views/Birdwatching.jsx
@@ -1,12 +1,11 @@
 import BirdwatchingTable from "../components/BirdWatchingTable/BirdWatchingTable";
 
-
 export default function Birdwatching(props) {
 
     return (
         <main className="container birdwatchingPage">
             <h3>Hola {props.userSession.name}, ¿Algún avistamiento hoy?</h3>
-            <BirdwatchingTable />
+            <BirdwatchingTable/>
         </main>
     );
 }
